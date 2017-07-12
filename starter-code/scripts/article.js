53404af5c83c590c8768aea9e48e03b1420c74a4
Alana Franklin <alanafrankly@gmail.com>
2017-07-11 17:17:58 -0700

    Populated template

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,8 +3,6 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
-  // Save ALL the properties of `rawDataObj` into `this`
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
   this.author = rawDataObj.author;
@@ -13,23 +11,17 @@ function Article (rawDataObj) {
   this.body = rawDataObj.body;
 }
 
-
-
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.removeClass('template');
+  console.log($newArticle);
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
-
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
-  of the current template clone with properties from this particular Article instance.
-  We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
-    5. publication date. */
+  $newArticle.find('address a').text(this.author);
+  $newArticle.find('address a').attr('href',this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.article-body').html(this.body);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -38,12 +30,11 @@ Article.prototype.toHtml = function() {
 };
 
 rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 rawData.forEach(function(articleObject) {
-  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+
   articles.push(new Article(articleObject));
 });
 
